1308f9a7b1fea8c23a15874b5f0417453e4e4840
anitkumarsingh <anikumarsingh99@gmail.com>
2022-07-03 11:47:52 +0530

    bug fixed

--- practice/index.ts
+++ practice/index.ts
@@ -2,14 +2,19 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/2';
 
+interface TODO {
+id:number,
+title:string,
+completed:boolean
+}
 axios.get(url).then(res=>{
- const todos = res.data;
- const id = todos.ID;
- const title = todos.Title;
- const finished  = todos.finished
+ const todos = res.data as TODO;
+ const id = todos.id;
+ const title = todos.title;
+ const completed  = todos.completed
  console.log(`
    The TODOS has a ID of : ${id}
    has a tile of : ${title}
-   whether it has been completed of not ? : ${finished}
+   whether it has been completed of not ? : ${completed}
  `)
 })
