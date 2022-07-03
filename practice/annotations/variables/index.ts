e2090ebd555f2e39243bc71835793666ccde1c62
anitkumarsingh <anikumarsingh99@gmail.com>
2022-07-03 13:18:52 +0530

    annotations

---
+++ practice/annotations/variables/index.ts
@@ -0,0 +1,18 @@
+let num : number = 4
+let str :string = 'Anit'
+let now : Date = new Date()
+
+// Array
+let array :string[] =['apples','Orange'];
+
+// Object
+let obj:{x:number,status:boolean} = {
+  x:1,
+  status:true
+}
+
+// Function
+const myFun:(i:number)=>void = (i:number) =>{
+  console.log(i)
+}
+myFun(100);
