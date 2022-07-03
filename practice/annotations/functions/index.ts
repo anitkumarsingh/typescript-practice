19de9198afcfdea0dc3d59a8e48b49071360cedd
anitkumarsingh <anikumarsingh99@gmail.com>
2022-07-03 20:15:59 +0530

    more functions

--- practice/annotations/functions/index.ts
+++ practice/annotations/functions/index.ts
@@ -5,3 +5,11 @@ const add = (a: number, b: number) => {
 const subtract = (a: number, b: number): number => {
 	return a - b;
 };
+
+function multiple(a: number, b: number): number {
+	return a * b;
+}
+
+const divide = function (a: number, b: number): number {
+	return a / b;
+};
