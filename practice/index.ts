8a85f7c76b890975cefe9fc67a37ed00e94d4933
anitkumarsingh <anikumarsingh99@gmail.com>
2022-07-03 11:44:18 +0530

    making bugs

--- practice/index.ts
+++ practice/index.ts
@@ -4,10 +4,12 @@ const url = 'https://jsonplaceholder.typicode.com/todos/2';
 
 axios.get(url).then(res=>{
  const todos = res.data;
- const {id,title,completed} = todos;
+ const id = todos.ID;
+ const title = todos.Title;
+ const finished  = todos.finished
  console.log(`
    The TODOS has a ID of : ${id}
    has a tile of : ${title}
-   whether it has been completed of not ? : ${completed}
+   whether it has been completed of not ? : ${finished}
  `)
 })
