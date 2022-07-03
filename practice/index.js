064d5f19236351ac9ff0633947d7a9a566efc736
anitkumarsingh <anikumarsingh99@gmail.com>
2022-07-03 11:35:28 +0530

    prettier printing

--- practice/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var axios_1 = require("axios");
-var url = 'https://jsonplaceholder.typicode.com/todos/2';
-axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
-});
--- practice/index.ts
+++ practice/index.ts
@@ -3,5 +3,11 @@ import axios from "axios";
 const url = 'https://jsonplaceholder.typicode.com/todos/2';
 
 axios.get(url).then(res=>{
-  console.log(res.data);
+ const todos = res.data;
+ const {id,title,completed} = todos;
+ console.log(`
+   The TODOS has a ID of : ${id}
+   has a tile of : ${title}
+   whether it has been completed of not ? : ${completed}
+ `)
 })
