466a30be86c0f63057ca9cb4350c678ad73935d6
anitkumarsingh <anikumarsingh99@gmail.com>
2022-07-05 18:02:37 +0530

    inheritance with fields

--- practice/annotations/classes/fields.ts
+++ practice/annotations/classes/fields.ts
@@ -7,5 +7,17 @@ class Feature {
 	}
 }
 
-const jeep = new Feature('orange');
-console.log(jeep.color);
+class Maruthi extends Feature {
+	constructor(public wheels: number, color: string) {
+		super(color);
+	}
+}
+
+const jeep = new Maruthi(4, 'orange');
+console.log(jeep.color, jeep.wheels);
+
+
+
+
+
+
